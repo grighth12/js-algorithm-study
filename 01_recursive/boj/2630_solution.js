8a6984de5911de687b11a9d68a549b9bca5ea287
jeonghee <grighth12@gmail.com>
2022-01-11 03:17:27 +0900

    색종이 만들기 해결

--- 01_recursive/boj/2630_solution.js
+++ 01_recursive/boj/2630_solution.js
@@ -1,5 +1,17 @@
-// const input = fs.readFileSync("/dev/stdin").toString().split("\n");
-// const arr = input.map((str) => str.split(" ").map((char) => +char));
+const input =
+  process.platform === "linux"
+    ? require("fs").readFileSync("/dev/stdin").toString().split("\n")
+    : [
+        "8",
+        "1 1 0 0 0 0 1 1",
+        "1 0 1 0 0 0 1 1",
+        "0 0 0 0 1 1 0 0",
+        "0 0 0 0 1 1 0 0",
+        "1 0 0 0 1 1 1 1",
+        "0 1 0 0 1 1 1 1",
+        "0 0 1 1 1 1 0 1",
+        "0 0 1 1 1 1 1 1",
+      ];
 
 const recursive = (x, y, n) => {
   if (n === 1) {
@@ -11,36 +23,70 @@ const recursive = (x, y, n) => {
     const c = recursive(x, y + len, len);
     const d = recursive(x + len, y + len, len);
 
-    // NOTE 합으로 확인하면 안 됨, w 1, b 3으로 이루어진 len = 8 짜리 색종이가 있을 경우 w가
-    // 모두 1인지 조건으로 확인해야 색종이가 한장으로 합쳐지는지 알 수 있음
-    const white = a.white + b.white + c.white + d.white;
-    const blue = a.blue + b.blue + c.blue + d.blue;
+    // NOTE 전체 면이 white인지 blue인지 체크하는 조건문에서 실수 자주 함
+    // 1. a.white ~ d.white 더한 값이 4인가? 로 확인했는데 예외가 있었음 w:2, b:2 / w: 2, b: 2 / w:0, b:0 / w:0, b:0 인 경우 w:4, b:4 여야 하지만 초기 조건으로 확인할 경우 w: 0, b: 0이 됨
+    // 2. 문제 확인 후 a.white ~ d.white가 모두 1인가?로 확인했는데 이 역시 w:1, b:3 / w:1, b:3 / w:1, b:3 / w:1, b:3 인 경우 w:4, b: 12 여야 하지만 해당 조건으로 확인할 경우 w: 1, b: 4가 됨
+    // 3. 결론적으로 모든 면이 하얀색인지 확인하려면 모두 w:1 b:0으로 이루어져 있는지 검사해야 함
+    const white =
+      a.white === 1 &&
+      a.blue === 0 &&
+      b.white === 1 &&
+      b.blue === 0 &&
+      c.white === 1 &&
+      c.blue === 0 &&
+      d.white === 1 &&
+      d.blue === 0
+        ? 1
+        : a.white + b.white + c.white + d.white;
 
-    console.log(n);
-    console.log(
-      "white",
-      white === 4 ? 1 : white,
-      "blue",
-      blue === 4 ? 1 : blue
-    );
+    const blue =
+      a.white === 0 &&
+      a.blue === 1 &&
+      b.white === 0 &&
+      b.blue === 1 &&
+      c.white === 0 &&
+      c.blue === 1 &&
+      d.white === 0 &&
+      d.blue === 1
+        ? 1
+        : a.blue + b.blue + c.blue + d.blue;
+
     return {
-      white: white === 4 ? 1 : white,
-      blue: blue === 4 ? 1 : blue,
+      white,
+      blue,
     };
   }
 };
 
-const input = [
-  "1 1 0 0 0 0 1 1",
-  "1 1 0 0 0 0 1 1",
-  "0 0 0 0 1 1 0 0",
-  "0 0 0 0 1 1 0 0",
-  "1 0 0 0 1 1 1 1",
-  "0 1 0 0 1 1 1 1",
-  "0 0 1 1 1 1 1 1",
-  "0 0 1 1 1 1 1 1",
-];
+const arr = input.slice(1).map((str) => str.split(" ").map((char) => +char));
 
-const arr = input.map((str) => str.split(" ").map((char) => +char));
+const result = recursive(0, 0, input[0]);
+console.log(result.white);
+console.log(result.blue);
 
-console.log(recursive(0, 0, arr[0].length));
+// * 게시판에서 찾고 시도해본 반례들
+//   [
+//     "8",
+//     "1 1 0 0 0 0 1 1",
+//     "1 0 1 0 0 0 1 1",
+//     "0 0 0 0 1 1 0 0",
+//     "0 0 0 0 1 1 0 0",
+//     "1 0 0 0 1 1 1 1",
+//     "0 1 0 0 1 1 1 1",
+//     "0 0 1 1 1 1 0 1",
+//     "0 0 1 1 1 1 1 1",
+//   ]
+
+// ["4", "1 1 1 1", "1 1 1 1", "1 1 1 1", "1 1 1 1"]
+
+//   [
+//     "8",
+//     "1 1 0 0 0 0 1 1",
+//     "1 1 0 0 0 0 1 1",
+//     "0 0 0 0 1 1 0 0",
+//     "0 0 0 0 1 1 0 0",
+//     "1 0 0 0 1 1 1 1",
+//     "0 1 0 0 1 1 1 1",
+//     "0 0 1 1 1 1 1 1",
+//     "0 0 1 1 1 1 1 1",
+//   ];
