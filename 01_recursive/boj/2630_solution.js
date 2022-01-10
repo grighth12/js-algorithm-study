02b03b3cd181d3dbca5e95ba508c70d446345e99
jeonghee <grighth12@gmail.com>
2022-01-11 02:17:51 +0900

    하얀색, 파란색 구분해서 셀 경우 조건실수 한 코드

--- 01_recursive/boj/2630_solution.js
+++ 01_recursive/boj/2630_solution.js
@@ -1,24 +1,32 @@
 // const input = fs.readFileSync("/dev/stdin").toString().split("\n");
-// const arr = input.map((str) => str.split(" ")).map((char) => +char);
+// const arr = input.map((str) => str.split(" ").map((char) => +char));
 
 const recursive = (x, y, n) => {
-  //   console.log(n);
-  if (n === 2) {
-    if (
-      (arr[x][y] + arr[x][y + 1] + arr[x + 1][y] + arr[x + 1][y + 1]) % 4 ===
-      0
-    ) {
-      return 1;
-    } else {
-      return 4;
-    }
+  if (n === 1) {
+    return arr[x][y] === 1 ? { blue: 1, white: 0 } : { white: 1, blue: 0 };
   } else {
-    return (
-      recursive(0, 0, n / 2) +
-      recursive(0, n / 2, n / 2) +
-      recursive(n / 2, 0, n / 2) +
-      recursive(n / 2, n / 2, n / 2)
+    const len = n / 2;
+    const a = recursive(x, y, len);
+    const b = recursive(x + len, y, len);
+    const c = recursive(x, y + len, len);
+    const d = recursive(x + len, y + len, len);
+
+    // NOTE 합으로 확인하면 안 됨, w 1, b 3으로 이루어진 len = 8 짜리 색종이가 있을 경우 w가
+    // 모두 1인지 조건으로 확인해야 색종이가 한장으로 합쳐지는지 알 수 있음
+    const white = a.white + b.white + c.white + d.white;
+    const blue = a.blue + b.blue + c.blue + d.blue;
+
+    console.log(n);
+    console.log(
+      "white",
+      white === 4 ? 1 : white,
+      "blue",
+      blue === 4 ? 1 : blue
     );
+    return {
+      white: white === 4 ? 1 : white,
+      blue: blue === 4 ? 1 : blue,
+    };
   }
 };
 
@@ -35,5 +43,4 @@ const input = [
 
 const arr = input.map((str) => str.split(" ").map((char) => +char));
 
-console.log(arr);
 console.log(recursive(0, 0, arr[0].length));
