c1457a543fbc9f86e8d8c16805b4a8d0007fd055
jeonghee <grighth12@gmail.com>
2022-02-02 03:58:02 +0900

    백준 0만들기 해결

--- 02_brute-force/boj/7490_makeZero.js
+++ 02_brute-force/boj/7490_makeZero.js
@@ -10,26 +10,19 @@ input = input.split("\n").slice(1);
 let answer = "";
 
 for (let i = 0; i < input.length; i++) {
-  let res = [[1]];
+  let res = ["1"];
   for (let j = 2; j <= input[i]; j++) {
     const len = res.length;
     for (let k = 0; k < len; k++) {
-      res = [
-        ...res,
-        [...res[k], " ", j],
-        [...res[k], "+", j],
-        [...res[k], "-", j],
-      ];
+      res = [...res, res[k] + " " + j, res[k] + "+" + j, res[k] + "-" + j];
     }
+
+    res = res.slice(len);
   }
 
-  const tmp = res
-    .filter((r) => r.length === input[i] * 2 - 1)
-    .map((r) => r.join(""));
-
-  for (let j = 0; j < tmp.length; j++) {
-    if (eval(tmp[j].replace(/(\s*)/g, "")) === 0) {
-      answer += `${tmp[j]}\n`;
+  for (let j = 0; j < res.length; j++) {
+    if (eval(res[j].replace(/(\s*)/g, "")) === 0) {
+      answer += `${res[j]}\n`;
     }
   }
 
